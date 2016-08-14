163ccdd8ae79f592ca26f4a6eb94b919e61e1213
hendrikniemann <h.neverman@web.de>
2016-08-15 00:15:17 +0200

    Add first routing with CSA from real data
    First routing implementation with std output for GTFS data

--- src/main/scala/GTFSReader.scala
+++ src/main/scala/GTFSReader.scala
@@ -1,13 +1,21 @@
-class GTFSData(val stops: Map[Int, Stop], val connections: List[Connection]) {
+case class TripConnection(
+                           depStation: Int,
+                           arrStation: Int,
+                           depTime: Int,
+                           arrTime: Int,
+                           trip: Trip
+                         ) extends Connection
+
+class GTFSData(val stops: Map[Int, Stop], val connections: List[TripConnection], val routes: Map[Int, Route]) {
   def findStopByName(name: String): Option[Stop] = stops find { _._2.name == name } map { _._2 }
 }
 
 object GTFSData {
-  private def makeConnectionsFromStops(timedStops: List[StopTime]): List[Connection] = timedStops match {
+  private def makeConnectionsFromStops(trip: Trip)(timedStops: List[StopTime]): List[TripConnection] = timedStops match {
     case _ :: Nil => List()
     case Nil => List()
     case from :: to :: rest =>
-      Connection(from.stopId, to.stopId, from.departureTime, to.arrivalTime) :: makeConnectionsFromStops(to :: rest)
+      TripConnection(from.stopId, to.stopId, from.departureTime, to.arrivalTime, trip) :: makeConnectionsFromStops(trip)(to :: rest)
   }
 
   def fromDirPath(path: String): GTFSData = {
@@ -15,29 +23,44 @@ object GTFSData {
     val calendarData = CalendarDateReader.read(path + "calendar_dates.txt")
     val stopTimeData = StopTimeReader.read(path + "stop_times.txt")
     val tripData = TripReader.read(path + "trips.txt").toList.groupBy(t => t.serviceId)
+    val routeData = RouteReader.read(path + "routes.txt")
 
     val stops = stopData.foldLeft(Map[Int, Stop]())((p: Map[Int, Stop], n: Stop) => p + (n.id -> n))
+    val routes = routeData.foldLeft(Map[Int, Route]())((p: Map[Int, Route], n: Route) => p + (n.id -> n))
     val stopTimes = stopTimeData.toList.groupBy(s => s.tripId)
 
     val todaysConnections = calendarData filter { _.date == 20161108 }
 
-    println(todaysConnections.size)
+    val connections: List[TripConnection] = todaysConnections.toList flatMap {
+      (operationDate: CalendarDate) => {
+        // If a service operates on a date all trips of the service operate on this date
+        // we then have to go through all of these trips and add their connections
+        val trips: List[Trip] = tripData.getOrElse(operationDate.serviceId, List())
 
-    val connections = for (date <- todaysConnections) {
-      val trips: List[Trip] = tripData.getOrElse(date.serviceId, List())
-
-      trips foreach {
-        trip => stopTimes.get(trip.tripId) match {
-          case Some()
+        trips flatMap {
+          trip => stopTimes.get(trip.id) match {
+            case Some(l) => makeConnectionsFromStops(trip)(l)
+            case None => List()
+          }
         }
       }
     }
 
-    new GTFSData(stops, List())
+    new GTFSData(stops, connections, routes)
   }
 
   def main(args: Array[String]): Unit = {
-    val data = fromDirPath("D:\\Users\\hendrikniemann\\Documents\\gtfsdata\\fv\\")
-    data.findStopByName("Frankfurt(Main)Hbf").foreach(println)
+    val data = fromDirPath("C:/Users/Hendrik/Documents/Uni/Bachelorarbeit/testdata/")
+    val ffm = data.findStopByName("Frankfurt(Main)Hbf").get
+    val berlin = data.findStopByName("Berlin Hbf").get
+
+    val res = Csa.find(data.connections, Query(ffm.id, berlin.id, 6000))
+
+    val resolveStop: Int => String = data.stops(_).name
+    val resolveRoute: Int => String =  data.routes(_).longName
+
+    res map {
+      c => s"${resolveStop(c.depStation)} => ${resolveStop(c.arrStation)} (${resolveRoute(c.trip.routeId)})"
+    } foreach { println }
   }
 }
