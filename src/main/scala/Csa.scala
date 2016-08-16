e4a8baecbbbbd853b259a2c3842f26730ac83d4b
hendrikniemann <h.neverman@web.de>
2016-08-16 16:53:40 +0200

    Add one to one query loop optimization
    The loop will now exit, when the scanned connection goes beyond the
    earliest arrival time at the target stop

--- src/main/scala/Csa.scala
+++ src/main/scala/Csa.scala
@@ -8,23 +8,23 @@ case class BasicConnection(depStation: Int, arrStation: Int, depTime: Int, arrTi
 case class Query(depStation: Int, arrStation: Int, depTime: Int)
 
 object Csa {
-  val infinity = BasicConnection(0, 0, 0, Int.MaxValue)
-
   def makeConnection[C <: Connection](shortest: Map[Int, C], start: Int, end: Int): List[C] = {
-    if (end == start)
-      List()
-    else
-      shortest.get(end) match {
-        case Some(connection) =>
-          connection :: makeConnection(shortest, start, connection.depStation)
-        case None =>
-          throw new Exception("Connection not found in map!")
-      }
+    if (end == start) List() else shortest.get(end) match {
+      case Some(connection) =>
+        connection :: makeConnection(shortest, start, connection.depStation)
+      case None =>
+        throw new Exception("Connection not found in map!")
+    }
   }
 
   def find[C <: Connection](connections: Array[C], query: Query): List[C] = {
-    var shortest = Map[Int, C]()
-    for (conn <- connections) {
+    val infinity = BasicConnection(0, 0, 0, Int.MaxValue)
+
+    var shortest: Map[Int, C] = Map[Int, C]()
+    var i = 1
+    // since this calculates one to one queries we can break when the connection departs later then EAT at target stop
+    while (i < connections.length && shortest.getOrElse(query.arrStation, infinity).arrTime > connections(i).depTime) {
+      val conn = connections(i)
       if (
         conn.depStation == query.depStation && query.depTime < conn.depTime ||
           shortest.getOrElse(conn.depStation, infinity).arrTime < conn.depTime
@@ -36,6 +36,7 @@ object Csa {
             shortest = shortest + (conn.arrStation -> conn)
         }
       }
+      i += 1
     }
     makeConnection(shortest, query.depStation, query.arrStation).reverse
   }
