a6b30c20f6e5a7fb9464d8f2c40128a536c58771
hendrikniemann <h.neverman@web.de>
2016-08-19 14:25:28 +0200

    Create small REST service for timetable queries
    Accessing queries with a REST service on a preloaded dataset is easier
    than compiling new for every query.

--- build.sbt
+++ build.sbt
@@ -4,8 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-mainClass in (Compile, run) := Some("cli.Entry")
+mainClass in (Compile, run) := Some("rest.Endpoint")
 
 scalacOptions += "-target:jvm-1.8"
 
 libraryDependencies += "com.nrinaudo" %% "kantan.csv" % "0.1.13"
+libraryDependencies += "org.analogweb" %% "analogweb-scala" % "0.9.12"
---
+++ src/main/scala/rest/Endpoint.scala
@@ -0,0 +1,63 @@
+package rest
+
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+import algorithm.{Csa, Query}
+import gtfs._
+import org.analogweb.core.Servers
+import org.analogweb.scala.Analogweb
+
+object Endpoint extends Analogweb {
+  var data = GTFSData.empty
+
+  def denseConnection(l: List[TripConnection]): List[TripConnection] = l match {
+    case Nil => Nil
+    case x :: xs => denseConnection(xs) match {
+      case y :: ys if x.trip == y.trip =>
+        TripConnection(x.depStation, y.arrStation, x.depTime, y.arrTime, x.trip) :: ys
+      case ys => x :: ys
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Please enter location of GTFS data folder")
+    val dir = scala.io.StdIn.readLine("GTFS location: ")
+    println("Initializing GTFS feed, this might take a while...")
+    data = GTFSData.fromDirPath(dir)
+    Servers.create(8080).run()
+  }
+
+  get("/query/{from}/{to}") { implicit r =>
+    def makeNiceConnection(tripConnection: TripConnection): NiceConnection = {
+      val departureStation = data.stops(tripConnection.depStation)
+      val arrivalStation = data.stops(tripConnection.arrStation)
+      val departureTime = GTFSData.epoch.plusMinutes(tripConnection.depTime).toString
+      val arrivalTime = GTFSData.epoch.plusMinutes(tripConnection.arrTime).toString
+      val name = data.routes(tripConnection.trip.routeId).longName
+
+      NiceConnection(departureStation, arrivalStation, departureTime, arrivalTime, name)
+    }
+
+    val now = LocalDateTime.of(2000, 1, 1, 0, 0).until(LocalDateTime.now(), ChronoUnit.MINUTES).toInt
+
+    val from = data.stops.get(param("from").toInt)
+    val to = data.stops.get(param("to").toInt)
+
+    val res = List(param("from"), param("to")) map { s => data.stops.get(s.toInt) } match {
+      case Some(start) :: Some(destination) :: Nil =>
+        Some(Csa.find(data.connections, Query(start.id, destination.id, now)))
+      case _ => None
+    }
+
+    res map {
+      denseConnection(_) map makeNiceConnection
+    } map asJson getOrElse BadRequest(asText("Could not find stops with those IDs."))
+  }
+
+  get("/stop/{id}") { implicit r =>
+    data.stops.get(param("id").toInt) map asJson getOrElse BadRequest(asText("Could not find a stop with that ID."))
+  }
+}
+
+case class NiceConnection(from: Stop, to: Stop, departureTime: String, arrivalTime: String, name: String)
